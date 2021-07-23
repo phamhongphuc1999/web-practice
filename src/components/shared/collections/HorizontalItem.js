e93dc4130fabcf9740e6a0f84b82c09cfa168ae2
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2021-07-23 17:55:07 +0700

    fix: Restaurant List detail

--- src/components/shared/collections/HorizontalItem.js
+++ src/components/shared/collections/HorizontalItem.js
@@ -1,6 +1,9 @@
 import { Card, CardContent, CardMedia } from '@material-ui/core'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import {ImPriceTag} from 'react-icons/im'
+import {BsTagFill} from 'react-icons/bs'
+import {RiMoneyDollarCircleFill} from 'react-icons/ri'
 
 const HorizontalItem = ({ data, ...prop }) => {
   const { t } = useTranslation()
@@ -10,13 +13,37 @@ const HorizontalItem = ({ data, ...prop }) => {
   }
 
   return (
-    <Card {...prop} onClick={() => itemClick()}>
-      <CardContent>
+    <Card {...prop} onClick={() => itemClick()} 
+    className="border-0 bg-transparent" style={{boxShadow: 'none'}}>
+      <CardContent className="grid grid-cols-12 gap-2">
         <CardMedia
+        className="col-span-3"
           component="img"
           image={data.image}
           alt={data.title}
         />
+        <div className="flex flex-col col-span-9 ml-3">
+          <div className="font-bold text-sm whitespace-nowrap overflow-ellipsis overflow-hidden">
+            {data.title}
+          </div>
+          <a href={`/bo-suu-tap/${data.metalink}`} className="text-xs text-blue-500">
+              {`${data.numberOfPlace} ${t('location')}`}
+            </a>
+            <div>
+              <div>
+                <ImPriceTag />
+                <span>{`${t('minimum')} ${data.minimum}`}</span>
+              </div>
+              <div>
+                <RiMoneyDollarCircleFill />
+                <span>{`${t('price')} ${data.price}`}</span>
+              </div>
+              <div>
+                <BsTagFill />
+                <span>{`${data.promotion}`}</span>
+              </div>
+            </div>
+        </div>
       </CardContent>
     </Card>
   )
--- src/components/shared/collections/RestaurantList.js
+++ src/components/shared/collections/RestaurantList.js
@@ -1,12 +1,13 @@
 import { Card, CardContent } from '@material-ui/core'
 import React, { useState } from 'react'
 import HorizontalItem from './HorizontalItem'
+import cx from 'classnames'
 
 const RestaurantList = ({ titles, actions }) => {
-  const [index, setIndex] = useState(0)
+  const [active, setActive] = useState(0)
 
   const actionClick = (index) => {
-
+    setActive(index)
   }
 
   return (
@@ -15,14 +16,17 @@ const RestaurantList = ({ titles, actions }) => {
         <div className="grid grid-cols-6 gap-1">
           <div className="col-span-4 grid grid-cols-4 gap-1">
             {titles.map((element, index) => (
-              <div key={index} onClick={() => actionClick(index)}>{element}</div>
+              <div key={index} onClick={() => actionClick(index)}
+              className={cx('cursor-pointer', {'text-blue-400': active === index})}>
+                {element}
+                </div>
             ))}
           </div>
           <div className="col-span-2">
           </div>
         </div>
-        <div>
-          {actions[index].map((element, index) => (
+        <div className="mt-5">
+          {actions[active].map((element, index) => (
             <HorizontalItem
               key={index} data={element}
             />
